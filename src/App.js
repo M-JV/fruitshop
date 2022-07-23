287d90094339a3703ee384449c55bbd191c5caae
M-JV <melvinjohn2001@gmail.com>
2022-07-23 15:29:15 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,40 @@
+import {useState} from "react";
 function App() {
+  const[items,setItems]=useState(["Mango🥭","Apple 🍎","Grapes 🍇"]);
+  const[itemNo,setItemNo]=useState(0);
+  const[itemInput,setItemInput]=useState('');
+  const currentfruit = items[itemNo];
+
+  const handleNextClick =()=>{
+    setItemNo(itemNo+1);
+  }
+
+
   return (
-    <div className="app">
+    
+    <><div className="app">
       <div className="app__container"></div>
-      <div className="app__heading"> Fruit Shop 
+      <div className="app__heading"> Fruit Shop
       </div>
       <div className="app__carousel">
-      
-      <span className="display--item">Apple 🍎</span>
+
+        <span className="display--item"> 
+        {currentfruit}
+        </span>
+        {/*      <span className="display--add">
+            <span>End of the list</span>
+            <input type="text" className="app__carousel--input"/> */}
+
       </div>
-    </div>
+      <div className="app__next">
+        <button onClick={handleNextClick} className="btn btn--primary">Next</button>
+      </div>
+      <div className="app__add">
+        <button className="btn btn--secondary">Add</button>
+      </div>
+    </div><div className="footer">
+        &copy;2022 HwR. Designed by MEJOVA
+      </div></>
     
   );
 }
